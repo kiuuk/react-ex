b6d868407d78abd952628dc19d95ab9cf343a0a9
Kay Ha <kiwook.ha@gmail.com>
2019-01-12 00:41:24 +0900

    added events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,24 +5,43 @@ class Counter extends Component {
     count: 0,
     tags: ["tag1", "tag2", "tag3"]
   };
+
   render() {
     let classes = this.getBadgeClasses();
 
     return (
       <React.Fragment>
-        <span className={classes}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">+</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <span style={{ minWidth: 60, lineHeight: 2 }} className={classes}>
+          {this.formatCount()}
+        </span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          +
+        </button>
+        <div>{this.listCondition()}</div>
       </React.Fragment>
     );
   }
 
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  listCondition() {
+    if (this.state.tags.length === 0) return "No tags";
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
   getBadgeClasses() {
-    let classes = "badge m-2 ";
+    let classes = "badge mr-2 ";
     classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
     return classes;
   }
