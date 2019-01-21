83d76f91983347fec70b0ed68baa92ffd7bfc703
Kay Ha <kiwook.ha@gmail.com>
2019-01-21 23:11:05 +0900

    passing data

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,13 +3,13 @@ import Counter from "./counter";
 
 class Counters extends Component {
   state = {
-    counters: [{ id: 1, value: 0 }, { id: 2, value: 0 }, { id: 3, value: 0 }]
+    counters: [{ id: 1, value: 3 }, { id: 2, value: 0 }, { id: 3, value: 0 }]
   };
   render() {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} />
+          <Counter key={counter.id} value={counter.value} />
         ))}
       </div>
     );
